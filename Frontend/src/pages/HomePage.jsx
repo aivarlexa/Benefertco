43a4d8ac607ba58fd43371701bd98de4d8412678
Prachi Nadgir <nadgirprachi@gmail.com>
2026-08-20 13:34:27 +0530

    fix: remove unused import of Statistics component from HomePage

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import CTASection from '../components/CTASection';
 import Hero from '../components/Hero';
 import Innovation from '../components/Innovation';
-import Statistics from '../components/Statistics';
 
 export default function HomePage() {
   return (
